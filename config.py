12a42b7f7b21a5af050b2e31451100ab2b5f6f3c
VargKernel <sbox.simple@protonmail.com>
2026-04-01 20:44:34 +0300

    Enhance Settings class with Redis URL and extra config
    Added REDIS_URL with default value and updated model_config to ignore extra fields.

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 
-
 class Settings(BaseSettings):
-	URL_TOTAL: str
+    URL_TOTAL: str
+    REDIS_URL: str = "redis://localhost:6379"
 
-	model_config = SettingsConfigDict(
-		env_file=os.path.join(os.path.dirname(__file__), ".env")
-	)
-
+    model_config = SettingsConfigDict(
+        env_file=os.path.join(os.path.dirname(__file__), ".env"),
+        extra='ignore'
+    )
 
 settings = Settings()
