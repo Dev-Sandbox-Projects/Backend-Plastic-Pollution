aa21e48bbb624b5be8df43e601ab10ff67cbcabd
VargKernel <sbox.simple@protonmail.com>
2026-04-01 19:39:52 +0300

    added environment variable for localhost fallback

--- app/parser.py
+++ app/parser.py
@@ -9,7 +9,7 @@ from io import StringIO
 from config import settings
 import os
 
-redis_url = os.getenv("REDIS_URL")
+redis_url = os.getenv("REDIS_URL", "redis://localhost:6379")
 r = redis.from_url(redis_url, decode_responses=True)
 
 @asynccontextmanager
