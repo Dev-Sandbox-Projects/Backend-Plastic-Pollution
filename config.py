7ca5d700576096877d06fce5525a39a7b9bb0f88
VargKernel <sbox.simple@protonmail.com>
2026-04-01 21:49:34 +0300

    Refactor Settings class in config.py
    Removed unnecessary line breaks in Settings class.

--- config.py
+++ config.py
@@ -2,12 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 import os
 
 class Settings(BaseSettings):
-	URL_TOTAL: str
-	REDIS_URL: str = "redis://localhost:6379/0"
-	model_config = SettingsConfigDict(
-		env_file=os.path.join(os.path.dirname(__file__), ".env")
-	)
-
+    URL_TOTAL: str
+    REDIS_URL: str = "redis://localhost:6379/0"
+    
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), ".env"),
         extra='ignore'
