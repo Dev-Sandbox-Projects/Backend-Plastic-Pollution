0d711039abf26ffe6f656680fb4c44ffe7ce141d
REvDi <peresadok33@gmail.com>
2026-04-01 21:13:06 +0300

    feat: setup redis connection and pydantic config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 class Settings(BaseSettings):
 	URL_TOTAL: str
-
+	REDIS_URL: str = "redis://localhost:6379/0"
 	model_config = SettingsConfigDict(
 		env_file=os.path.join(os.path.dirname(__file__), ".env")
 	)
