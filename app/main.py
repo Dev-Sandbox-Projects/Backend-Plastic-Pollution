c98d5d41fddbfb3f8fea3852bd22281bb33a58e0
VargKernel <sbox.simple@protonmail.com>
2026-04-01 22:27:15 +0300

    Implement CORS middleware for frontend access
    Added CORS middleware to allow requests from specified origins.

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # Добавлен импорт для CORS
 from app.parser import fetch_and_store
 from app.route import router
 
@@ -7,7 +8,8 @@ from app.route import router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
-        fetch_and_store()
+        # Парсинг данных при запуске приложения
+        fetch_and_store() # 
         print("Date successful parsed")
     except Exception as e:
         print(f"Failed: {e}")
@@ -15,4 +17,19 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-app.include_router(router)
+
+# Настройка CORS для разрешения запросов с вашего фронтенда
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://vargkernel.github.io", 
+        "https://dev-sandbox-projects.github.io",
+        "http://localhost:5173",                   # Для локальной разработки (Vite)
+        "http://localhost:3000",                   # Для локальной разработки (другие порты)
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
+
+app.include_router(router) # Подключение маршрутов из route.py
