41a57009f208ef44b201b4ddbf1015d5b111a72e
VargKernel <sbox.simple@protonmail.com>
2026-04-03 22:57:00 +0300

    Added method HEAD

--- app/route.py
+++ app/route.py
@@ -3,25 +3,26 @@ from app.database import db
 
 router = APIRouter(tags=["Plastic Statistics"])
 
-
-@router.get("/", status_code=status.HTTP_200_OK)
+# Добавлен метод HEAD специально для Render Health Check
+@router.api_route("/", methods=["GET", "HEAD"], status_code=status.HTTP_200_OK)
 async def successful_response():
-	return {"message": "Server running successful"}
+    return {"status": "ok", "message": "Server running successful"}
 
 
 @router.get("/stats/plastic", status_code=status.HTTP_200_OK)
 async def get_plastic():
-	data = db.get("plastic_data")
-	if data:
-		return data
-	return {"debug": "Memory is empty, waiting for background task to fetch data..."}
+    data = db.get("plastic_data")
+    if data:
+        return data
+    return {"debug": "Memory is empty, waiting for background task to fetch data..."}
 
 
 @router.get("/stats/cards")
 async def get_cards():
-	data = db.get("plastic_cards")
-	if data:
-		return data
-	raise HTTPException(
-		status_code=status.HTTP_404_NOT_FOUND,
-		detail={"error": "data has not been loaded yet"})
+    data = db.get("plastic_cards")
+    if data:
+        return data
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail={"error": "data has not been loaded yet"}
+    )
